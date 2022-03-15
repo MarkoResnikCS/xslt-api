b5797bcf246f4d206cb46ce8ab2ed6ec951680a6
Marko Resnik <marko.resnik@igdsolutions.com>
2022-03-15 18:08:45 +0100

    xsl

--- src/test/java/com/igd/xsltapi/repository/TransformationRepositoryTest.java
+++ src/test/java/com/igd/xsltapi/repository/TransformationRepositoryTest.java
@@ -2,6 +2,7 @@ package com.igd.xsltapi.repository;
 
 import com.igd.xsltapi.entity.Transformation;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
